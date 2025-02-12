9ed98af0c83621af5bd5dd108ae0c768746dfec8
YounessFTN <youness.fatine1@gmail.com>
2025-02-13 00:24:01 +0100

    j'ai enfin trouvé l'erreur

--- api/index.ts
+++ api/index.ts
@@ -75,6 +75,4 @@ app.delete("/users/:id", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT} -> https://localhost:3000/`);
-});
+export default app;
