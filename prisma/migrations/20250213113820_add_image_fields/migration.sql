7c9fa48c1b1d94d6067c36c500a9f9759f47c39b
YounessFTN <youness.fatine1@gmail.com>
2025-02-13 12:42:02 +0100

    add images on models

---
+++ prisma/migrations/20250213113820_add_image_fields/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Anime" ADD COLUMN     "image" TEXT;
+
+-- AlterTable
+ALTER TABLE "Blog" ADD COLUMN     "image" TEXT;
+
+-- AlterTable
+ALTER TABLE "Manga" ADD COLUMN     "image" TEXT;
